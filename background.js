4f7b71d2ecbc9597a15440f0406e4207ba61c085
Upama <upamatm@gmail.com>
2025-03-28 21:32:43 -0600

    Added floating button with magnifier icon, improve sidebar layout, and implemented naive summarizer now for prototype

--- background.js
+++ background.js
@@ -1,3 +1,9 @@
 chrome.action.onClicked.addListener((tab) => {
-  chrome.tabs.sendMessage(tab.id, { action: "toggleSidebar" });
+  chrome.tabs.sendMessage(tab.id, { action: "toggleSidebar" })
+    .then(response => {
+      console.log("Message sent:", response);
+    })
+    .catch((error) => {
+      console.warn("No content script found on this tab. Ignoring.");
+    });
 });
